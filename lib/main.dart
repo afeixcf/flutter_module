3220528c73305c89630f535b45042240bc64825a
xuechenfei <xuechenfei@ahaschool.com>
2019-10-06 18:12:03 +0800

    temp

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,81 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  // ShoppingCart sc = ShoppingCart('张三', '123456');
+  ShoppingCart(name: '张三')
+  ..bookings = [Item('苹果', 10.0), Item('鸭梨', 20.0)]
+  ..printInfo();
+  ShoppingCart.withCode(name: '李四', code: '123456')
+  ..bookings = [Item('苹果', 30.0), Item('鸭梨', 40.0)]
+  ..printInfo();
+
+  runApp(new MyApp());
+  if (Platform.isAndroid) {
+    // 以下两行 设置android状态栏为透明的沉浸。写在组件渲染之后，是为了在渲染后进行set赋值，覆盖状态栏，写在渲染之前MaterialApp组件会覆盖掉这个值。
+    SystemUiOverlayStyle systemUiOverlayStyle =
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent);
+    SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
+  }
+}
+
+// 定义商品 Item 类
+// class Item {
+//   double price;
+//   String name;
+//   Item(this.name, this.price);
+// }
+
+class Meta {
+  double price;
+  String name;
+  Meta(this.name, this.price);
+}
+
+class Item extends Meta {
+  Item(name, price) : super(name, price);
+
+  Item operator +(Item item) => Item(name + item.name, price + item.price);
+}
+abstract class PrintHelper {
+  printInfo() => print(getInfo());
+  getInfo();
+}
+
+// 定义购物车类
+class ShoppingCart extends Meta with PrintHelper{
+  String name;
+  DateTime date;
+  String code;
+  List<Item> bookings;
+
+  getInfos() {
+    return '''
+      1
+      2
+      3
+    ''';
+  }
+
+  double get price =>
+      bookings.reduce((value, element) => value + element).price;
+
+
+  ShoppingCart({name}) : this.withCode(name: name, code: null);
+  ShoppingCart.withCode({this.name, this.code})
+      : this.date = DateTime.now(), super(name, 0);
+
+  getInfo() => '''
+    购物车信息
+    ----------------------------------
+    用户用：$name,
+    优惠码：${code ?? '没有'},
+    总价:：$price,
+    Date：$date
+    ----------------------------------
+  ''';
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -45,6 +120,37 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
+  @override
+  void initState() {
+    super.initState();
+    print('init');
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    print('state change');
+  }
+
+  @override
+  void didUpdateWidget(oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    print('update widget');
+    print(oldWidget);
+  }
+
+  @override
+  void deactivate() {
+    super.deactivate();
+    print('暂时移除');
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    print('永久销毁');
+  }
+
   void _incrementCounter() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
@@ -53,6 +159,7 @@ class _MyHomePageState extends State<MyHomePage> {
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
+      print('setState');
     });
   }
 
